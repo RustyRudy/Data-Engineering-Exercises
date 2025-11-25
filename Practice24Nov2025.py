9d9befb5abc1b95b91d7858787c4ce5efe61bcc9
Prakash Gurung <49418845+RustyRudy@users.noreply.github.com>
2025-11-24 20:25:53 -0700

    Add files via upload
    some after class python problems

---
+++ Practice24Nov2025.py
@@ -0,0 +1,26 @@
+def anagramCompare(string1, string2):
+    aDict = {}
+    bDict = {}
+
+    for i in string1:
+        if i in aDict: aDict[i] += 1
+        else: aDict[i] = 1
+    for j in string2:
+        if j in bDict: bDict[j] += 1
+        else: bDict[j] = 1
+
+    print("aKeys:", list(aDict.keys()))
+    print("aValues:", list(aDict.values()))
+    print("bKeys:", list(bDict.keys()))
+    print("bValues:", list(bDict.values()))
+
+    if aDict == bDict: print("They're anagrams")
+    else: print("They're not anagrams")
+
+
+if __name__ == '__main__':
+
+    aString = "net"
+    bString = "ten"
+    anagramCompare(aString, bString)
+
