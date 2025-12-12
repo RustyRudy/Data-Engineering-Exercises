913281bd384e49919ceeb93ac8c5b253c5f8e31d
Prakash Gurung <49418845+RustyRudy@users.noreply.github.com>
2025-12-11 21:03:33 -0700

    Update PySparkClasProj5.py

--- PySpark/PySparkClasProj5.py
+++ PySpark/PySparkClasProj5.py
@@ -7,4 +7,5 @@ def classProj5(spark: SparkSession):
     print("Rows in the books.json file:", df.count())
     print("Distinct Rows in the books.json file:", df.distinct().count())
     df_dropDuplicates = df.dropDuplicates()
+    #gotta get that contri
     print(" Rows in the books.json file after dropping duplis:", df_dropDuplicates.count())
