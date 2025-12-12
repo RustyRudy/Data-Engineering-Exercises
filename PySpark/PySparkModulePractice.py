20377d8adae9962116f1076f17ba13f5bfceede4
Prakash Gurung <49418845+RustyRudy@users.noreply.github.com>
2025-12-11 21:04:02 -0700

    Update PySparkModulePractice.py

--- PySpark/PySparkModulePractice.py
+++ PySpark/PySparkModulePractice.py
@@ -1,8 +1,9 @@
 from pyspark.sql import SparkSession
-
+#gotta get that contri
 
 def sqlQueriesDF(spark: SparkSession):
     parDF = spark.read.parquet("file:///tmp/output/people.parquet")
     parDF.createOrReplaceTempView("ParquetTable")
     parkSQL = spark.sql("select * from ParquetTable where salary >= 4000 ")
+
     parkSQL.show()
